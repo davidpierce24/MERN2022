44edc4db684c2a2dcf8365cf4e36d573de78f94e
davidpierce24 <pie2ce@gmail.com>
2022-07-05 16:01:51 -0400

    super ninja

---
+++ SuperNinja/superNinja.js
@@ -0,0 +1,40 @@
+class Ninja {
+    constructor(name) {
+        this.name = name;
+        this.health = 10;
+        this.speed = 3;
+        this.strength = 3;
+    }
+
+    sayName(){
+        console.log(`${this.name}`);
+    }
+
+    showStats() {
+        console.log(`${this.name}`)
+        console.log(`strength: ${this.strength}`)
+        console.log(`speed: ${this.speed}`)
+        console.log(`health: ${this.health}`)
+    }
+    
+    drinkSake() {
+        this.health += 10;
+    }
+}
+
+const ninja1 = new Ninja("Ichigo")
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
+ninja1.showStats();
+
+
+class Sensei extends Ninja {
+    constructor(name){
+        this.name = name;
+        this.health = 200;
+        this.speed = 10;
+        this.strength = 10;
+        this.wisdom = 10;
+    }
+}

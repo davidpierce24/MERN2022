6c80718e1a81f962760c8c908763fe67e5d5ec0b
davidpierce24 <pie2ce@gmail.com>
2022-07-06 18:47:21 -0400

    updates

--- putting-it-together/src/components/PersonCard.jsx
+++ putting-it-together/src/components/PersonCard.jsx
@@ -1,14 +1,32 @@
-import Component from "react";
+import {Component} from "react";
 
 class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        btnclick : props.age,
+        // name : props.firstName
+        }
+    }
+
+    // birthday = () => {
+    //     this.setState((state) => {
+    //         return {btnclick: state.btnclick + 1, name: state.name + "y"}
+    //     })
+        
+    // }
+    
+    birthday = () => this.setState((state) => ({btnclick: state.btnclick + 1}))
+
     render() {
         return(
             <div>
                 <h1>{this.props.lastName}, {this.props.firstName}</h1>
-                <p>Age: {this.props.age}</p>
+                <p>Age: {this.state.btnclick}</p>
                 <p>Hair Color: {this.props.hairColor}</p>
+                <button onClick={this.birthday}>Birthday Button for {this.props.firstName} {this.props.lastName}</button>
             </div>
-        )
+        );
     }
 }
 

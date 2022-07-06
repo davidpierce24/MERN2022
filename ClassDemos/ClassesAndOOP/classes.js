133662e47684175f6744b31e0fd4974b009d7470
davidpierce24 <pie2ce@gmail.com>
2022-07-06 11:31:28 -0400

    updates

--- ClassDemos/ClassesAndOOP/classes.js
+++ ClassDemos/ClassesAndOOP/classes.js
@@ -18,7 +18,32 @@ class Character {
         console.log(`${target.name} now has ${target.health} HP`)
     }
 }
-const char1 = new Character("Ichigo", 100, 50, 2);
+
+// introducing inheritance
+class MagicBoy extends Character {
+    constructor(name, health, stength, intelligence, mana) {
+        super(name, health, stength, intelligence);
+        this.mana = mana;
+    }
+
+    attack(target) {
+        target.health -= this.intelligence;
+        this.mana -= 5;
+        console.log(`${this.name} slung a magic trash can at ${target.name} for ${this.intelligence} damage!`)
+        console.log(`${target.name} now has ${target.health} HP`)
+    }
+}
+
+
+
+
+
+const char1 = new Character("Ichigo", 600, 50, 2);
 const char2 = new Character("Kenpachi", 500, 40, -1);
 
 char1.attack(char2);
+
+
+const char3 = new MagicBoy("Orihime", 20, 2, 4, 50);
+
+char3.attack(char1);

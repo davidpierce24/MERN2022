08586f3e704fd34f8f5c63cdbb9733922037b227
davidpierce24 <pie2ce@gmail.com>
2022-07-06 16:48:19 -0400

    updates

---
+++ prop-it-up/src/components/PersonCard.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class PersonCard extends Component {
+    render(){
+        return(
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default PersonCard;

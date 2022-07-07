7a70fa91aa94cdca54c1031e1512d00ee26c4cad
davidpierce24 <pie2ce@gmail.com>
2022-07-07 12:02:25 -0400

    updates

--- ClassDemos/Components/class-component-lecture/src/components/First.jsx
+++ ClassDemos/Components/class-component-lecture/src/components/First.jsx
@@ -1,5 +1,17 @@
 import { Component } from "react";
 
+const darkMode = {
+    backgroundColor: "dimgray",
+    color: "ghostwhite", 
+    padding: "10px"
+}
+
+const lightMode = {
+    backgroundColor: "white",
+    color: "black",
+    padding: "10px"
+}
+
 class First extends Component {
     constructor(props){
         super(props);
@@ -10,11 +22,11 @@ class First extends Component {
 
     render() {
         return (
-            <>
+            <div style={this.state.light ? lightMode : darkMode}>
                 <h2>This is component 1</h2>
                 <h3>My name is {this.props.firstName}</h3>
                 <button onClick={ () => this.setState({light: !this.state.light}) }>{this.state.light ? "On" : "Off"}</button>
-            </>
+            </div>
         );
     }
 }

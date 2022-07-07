452a0578c4942ccbfc3d5c0d31b2a2c4c63f3813
davidpierce24 <pie2ce@gmail.com>
2022-07-07 11:28:43 -0400

    updates

--- ClassDemos/Components/class-component-lecture/src/App.jsx
+++ ClassDemos/Components/class-component-lecture/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import First from './components/First';
 
 function App() {
   return (
     <div className="App">
       <h1>Yo Yo Yo</h1>
+      <First/>
     </div>
   );
 }
---
+++ ClassDemos/Components/class-component-lecture/src/components/First.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class First extends Component {
+    render() {
+        return (
+            <>
+                <h2>This is component 1</h2>
+            </>
+        );
+    }
+}
+
+export default First;

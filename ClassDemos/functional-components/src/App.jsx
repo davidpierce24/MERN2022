6b29cefa3b9cc43c91766116a1f4d011a9f4b536
davidpierce24 <pie2ce@gmail.com>
2022-07-08 11:42:03 -0400

    updates

--- ClassDemos/functional-components/src/App.jsx
+++ ClassDemos/functional-components/src/App.jsx
@@ -1,10 +1,27 @@
 import './App.css';
 import First from './components/First';
+import { useState } from 'react';
 
 function App() {
+  const [light, setLight] = useState(true);
+
+  const lightMode = {
+    backgroundColor: "cyan",
+    color: "black",
+  }
+
+  const darkMode = {
+      backgroundColor: "darkblue",
+      color: "ghostwhite",
+  }
+
   return (
-    <div className="App">
+    <div className="App" style={light ? lightMode : darkMode}>
       <h2>Welcome to Functional Components</h2>
+      <button onClick={() => setLight(!light)}>{light ? "DarkMode" : "LightMode"}</button>
+      <First firstName = { "Bizmo" } lastName = { "Funions" } number = { 24 } />
+      <First firstName = { "Bizmo" } lastName = { "Funions" } number = { 24 } />
+      <First firstName = { "Bizmo" } lastName = { "Funions" } number = { 24 } />
       <First firstName = { "Bizmo" } lastName = { "Funions" } number = { 24 } />
     </div>
   );
--- ClassDemos/functional-components/src/components/First.jsx
+++ ClassDemos/functional-components/src/components/First.jsx
@@ -5,11 +5,28 @@ const First = (props) => {
 
     const { firstName, lastName, number } = props;
 
+    const lightMode = {
+        backgroundColor: "cyan",
+        color: "black",
+        padding: "10px",
+        width: "500px",
+        margin: "0 auto"
+    }
+    
+    const darkMode = {
+        backgroundColor: "darkblue",
+        color: "ghostwhite",
+        padding: "10px",
+        width: "500px",
+        margin: "0 auto"
+    }
+
     return (
-        <div>
+        <div style={light ? lightMode : darkMode}>
             <h2>This is my first component</h2>
             <h3>The name passed in was: { firstName } { lastName }</h3>
             <h4>Your number: { number }</h4>
+            <button onClick={() => setLight(!light)}>{light ? "Off" : "On"}</button>
         </div>
     )
 }

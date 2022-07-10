697ce468a247f93f13f9e8c702b1589bc61f8506
davidpierce24 <pie2ce@gmail.com>
2022-07-10 15:20:36 -0400

    updates

--- todo-list/src/components/TodoList.jsx
+++ todo-list/src/components/TodoList.jsx
@@ -12,44 +12,46 @@ const TodoList = props => {
         }
     }, [setList])
     
+
     const handleSubmit = (e) => {
         e.preventDefault();
         let newList = [...list, {item: e.target[0].value, todo: true, id: list.length+1}]
         setList(newList);
         e.target[0].value = "";
-        localStorage.setItem("list", JSON.stringify(list))
+        
+        setTimeout(store, 200)
+        
     }
     
+    
+    const store = () => localStorage.setItem("list", JSON.stringify(list))
     
     
     const finish = (idx) => {
         const filtered = list.map((item, i) => i === idx ? {...item, todo: !item.todo} : {...item});
         setList(filtered);
-        localStorage.setItem("list", JSON.stringify(list))
+        setTimeout(store, 200)
     }
     
     const removeItem = idx => {
         const removed = list.filter((item, i) =>{ return i !== idx })
         setList(removed);
-        localStorage.setItem("list", JSON.stringify(list))
+        setTimeout(store, 200)
     }
 
 
     return (
         <div>
-            <h1>Todo List</h1>
+            <h1>ToDo List</h1>
             <form onSubmit={handleSubmit}>
                 <label htmlFor="item">Add an Item to your Todo List </label> <br />
                 <input type="text" />
                 <input type="submit" value="Add to Todo List" style={{backgroundColor:"cyan"}} />
             </form>
-
-            
             <ul>
                 {
                     list.map( (item, i) => 
                     <Todo item={item} i={i} removeItem={removeItem} finish={finish}/>
-                    
                     )
                 }
             </ul>

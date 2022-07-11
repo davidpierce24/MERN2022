4ac8c1dc263df5a80caad02b7d0fa4541e629aac
davidpierce24 <pie2ce@gmail.com>
2022-07-11 11:07:59 -0400

    updates

--- Algos/w2/d1/algos.js
+++ Algos/w2/d1/algos.js
@@ -5,20 +5,28 @@
 
 function orderedIntersection(arr1, arr2) {
     let temp = [];
-    for(let i=0; i<arr1.length; i++){
-        for (let j = 0; j<arr2.length; j++){
-            if(arr2[j] > arr1[i]){
-                break;
-            }
-            if(arr1[i] === arr2[j]){
-                if(!temp.includes(arr1[i])){
-                    temp.push(arr1[i])
-                }
-            }
-        }
+    for(let i=0, j=0; i<arr1.length, j<arr2.length; i++, j++){
+        
     }
     return temp;
 }
+
+// function orderedIntersection(arr1, arr2) {
+//     let temp = [];
+//     for(let i=0; i<arr1.length; i++){
+//         for (let j = 0; j<arr2.length; j++){
+//             if(arr2[j] > arr1[i]){
+//                 break;
+//             }
+//             if(arr1[i] === arr2[j]){
+//                 if(!temp.includes(arr1[i])){
+//                     temp.push(arr1[i])
+//                 }
+//             }
+//         }
+//     }
+//     return temp;
+// }
 
 
 function unorderedIntersection(arr1, arr2) {
@@ -38,3 +46,8 @@ function unorderedIntersection(arr1, arr2) {
 
 console.log(orderedIntersection([1,2,2,3,4,5],[2,4,6,7,8]))
 console.log(orderedIntersection([1,1,1,2,3,4],[1,1,3,4,5]))
+
+// function arr() {
+//     let temp =[];
+//     for(let )
+// }

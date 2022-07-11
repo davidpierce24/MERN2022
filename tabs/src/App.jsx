e056cab5541f161295770b09b5c8d1cf5677b0a4
davidpierce24 <pie2ce@gmail.com>
2022-07-11 15:49:05 -0400

    updates

--- tabs/src/App.jsx
+++ tabs/src/App.jsx
@@ -5,14 +5,24 @@ import { useState } from 'react';
 
 
 function App() {
-  
-  const createTab = tab => {
-    setTab(tab);
+  const [display, setDisplay] = useState("");
+
+  const handleClick = (tab) => {
+    setDisplay(tab);
+    console.log(display)
   }
+
 
   return (
     <div className="App">
-      <Tab createTab={{"label":"Tab 1", "content":[1,2,4,4,6,76,7,6,4,3,3,2]}}/>
+      <div style={{display:"flex", gap: "10px", margin:"30px"}}>
+      <Tab createTab={{"label":"Tab 1", "content":[1,2,4,4,6,76,7,6,4,3,3,2]}} onClick={handleClick}/>
+      <Tab createTab={{"label":"Tab 2", "content":[1,2,4,4,6,76,7]}} onClick={handleClick}/>
+      <Tab createTab={{"label":"Tab 3", "content":[1,2,4]} } onClick={handleClick} />
+      </div>
+      <div>
+        {display.content}
+      </div>
     </div>
   );
 }
--- tabs/src/components/Tab.jsx
+++ tabs/src/components/Tab.jsx
@@ -1,14 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Tab = props => {
-    const [tab, setTab] = useState("");
+    const [tab, setTab] = useState(props.createTab);
+    const [clicked, setClicked] = useState(false);
 
-    
-
+    const handleClick = () => {
+        props.onClick(tab)
+        setClicked(true)
+    }
 
     return(
         <div>
-            
+            <button style={clicked ? {backgroundColor:"cyan"} : {backgroundColor:"white"}} onClick={handleClick}>{tab.label}</button>
         </div>
     )
 }

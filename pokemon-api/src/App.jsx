85ac8726be74d3fc91a5918e5af00426e11b91bd
davidpierce24 <pie2ce@gmail.com>
2022-07-12 01:20:57 -0400

    updates

--- pokemon-api/src/App.jsx
+++ pokemon-api/src/App.jsx
@@ -6,12 +6,18 @@ function App() {
   const [pokeList, setPokeList] = useState([]);
 
   const handleClick = (e) => {
-    fetch("https://pokeapi.co/api/v2/pokemon")
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
       .then(response => response.json())
-      .then(response => console.log(response))
       .then(response => setPokeList(response.results))
+      .then(response => console.log(response))
       .catch(err => console.log(err))
   }
+  
+  // const handleClick = (e) => {
+  //   fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
+  //           .then(response => response.json())
+  //           .then(response => setPokeList(response.results))
+  // }
 
   return (
     <div className="App">

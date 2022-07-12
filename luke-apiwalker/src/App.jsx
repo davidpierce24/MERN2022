897111f665e27969c5edfca5f14875123e975bf8
davidpierce24 <pie2ce@gmail.com>
2022-07-12 18:46:12 -0400

    updates

---
+++ luke-apiwalker/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css';
+import { useState } from 'react';
+import {Routes, Route, Link} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <form action="">
+        <div style={{display:"flex", justifyContent:"center", gap:"10px"}}>
+          <label htmlFor="searchFor">Search For: </label>
+          <select name="searchFor">
+            <option value="people">people</option>
+            <option value="planets">planets</option>
+          </select>
+          <label htmlFor="id">ID: </label>
+          <input type="number" />
+          <input type="submit" value="Search" />
+        </div>
+      </form>
+
+      <Routes>
+        <Route />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;

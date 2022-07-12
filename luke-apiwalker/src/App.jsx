499aeb3d90bb92b1b2e43d98d5620a62d4abace4
davidpierce24 <pie2ce@gmail.com>
2022-07-12 19:09:40 -0400

    updates

--- luke-apiwalker/src/App.jsx
+++ luke-apiwalker/src/App.jsx
@@ -1,19 +1,35 @@
 import './App.css';
 import { useState } from 'react';
 import {Routes, Route, Link} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [id, setId] = useState("");
+  const navigate = useNavigate();
+
+
+  const onSubmitHandler = e => {
+    e.preventDefault();
+    if(id == "people") {
+      navigate(`/people/${id}`)
+    }
+    if(id == "planets") {
+      navigate(`/planets/${id}`)
+    }
+  }
+
   return (
     <div className="App">
-      <form action="">
+      <form onSubmit={onSubmitHandler}>
         <div style={{display:"flex", justifyContent:"center", gap:"10px", margin:"20px"}}>
           <label htmlFor="searchFor">Search For: </label>
-          <select name="searchFor">
+          <select name="searchFor" onChange={ e => setSearch(e.target.value) }>
             <option value="people">people</option>
             <option value="planets">planets</option>
           </select>
           <label htmlFor="id">ID: </label>
-          <input type="number" />
+          <input type="number" onChange={ e => setId(e.target.value) } />
           <input type="submit" value="Search" style={{backgroundColor:"cyan"}}/>
         </div>
       </form>

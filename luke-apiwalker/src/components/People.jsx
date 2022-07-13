56aa3c319e83f17ebf497ca84ba5104ba7a951e4
davidpierce24 <pie2ce@gmail.com>
2022-07-12 23:17:21 -0400

    updates

--- luke-apiwalker/src/components/People.jsx
+++ luke-apiwalker/src/components/People.jsx
@@ -1,9 +1,11 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const People = props => {
     const [person, setPerson] = useState("");
+    const [homeworld, setHomeworld] = useState("");
     const { id } = useParams();
     // const {person} = props
 
@@ -13,6 +15,12 @@ const People = props => {
         .catch(err => console.log("These aren't the droids you're looking for"))
     }, [id])
 
+    useEffect(() => {
+        axios.get(`${person.homeworld}`)
+        .then(res => setHomeworld(res.data))
+        .catch(err => console.log("These aren't the droids you're looking for"))
+    }, [person])
+
 
     return (
         <div>
@@ -20,6 +28,7 @@ const People = props => {
             <h4>Height: {person.height}cm</h4>
             <h4>Weight: {person.mass}kg</h4>
             <h4>Eye Color: {person.eye_color}</h4>
+            <h4> <Link to={`/planets/${id}`} >Homeworld: {homeworld.name}</Link> </h4>
         </div>
     )
 }

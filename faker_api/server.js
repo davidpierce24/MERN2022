9bc74b2f1d6471646e6cca26538aaf76aab06b49
davidpierce24 <pie2ce@gmail.com>
2022-07-13 15:14:12 -0400

    updates

---
+++ faker_api/server.js
@@ -0,0 +1,7 @@
+import { express } from "express";
+import { faker } from '@faker-js/faker';
+
+const express = require("express");
+const faker = require("faker");
+const app = express();
+const port = 8000;

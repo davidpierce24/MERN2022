6908dcf841715f5a018bf7a7f9b5f1239c7800c5
davidpierce24 <pie2ce@gmail.com>
2022-07-14 17:17:02 -0400

    updates

--- ClassDemos/routing-demo/src/components/Contact.jsx
+++ ClassDemos/routing-demo/src/components/Contact.jsx
@@ -1,8 +1,50 @@
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+
 const Contact = props => {
+    const [name, setName] = useState("");
+    const [comment, setComment] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirm, setConfirm] = useState("");
+    const navigate = useNavigate();
+    
+    const sendSurvey = (e) => {
+        e.preventDefault();
+        navigate("/about")
+    }
+
+    const checkPasswords = e => {
+        if(password == confirm){
+            return true;
+        }
+        return false;
+    }
 
     return (
         <div>
-            Hi Contact
+            <h1>Hi Conatc</h1>
+            <form onSubmit={ sendSurvey }>
+                <div>
+                    <label>Your Name:</label>
+                    <input type="text" onChange={ (e) => setName(e.target.value) } value={ name } />
+                </div>
+                <div>
+                    <label>Your Comment:</label>
+                    <textarea onChange={ (e) => setComment(e.target.value) } value={ comment }></textarea>
+                </div>
+                <div>
+                    <label htmlFor="password">Password: </label>
+                    <input type="password" onChange={ e => setPassword(e.target.value) } />
+                </div>
+                <div>
+                    <label htmlFor="confirm">Confirm Password: </label>
+                    <input type="password" onChange={ e => {
+                        setConfirm(e.target.value) 
+                    }} />
+                </div>
+                {password == confirm ? "" : <p>Passwords much match</p>}
+                <input type="submit" value="Submit Survey" />
+            </form>
         </div>
     )
 }

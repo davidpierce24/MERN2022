95bc316a18d35ccd151a1aa19176a3758884ef55
davidpierce24 <pie2ce@gmail.com>
2022-07-14 19:37:07 -0400

    updates

---
+++ jokes-api/server/routes/jokes.routes.js
@@ -0,0 +1,10 @@
+const JokesController = require('../controllers/jokes.controller');
+
+module.exports = app => {
+    app.get('/api/jokes', JokesController.findAllJokes);
+    app.get('/api/jokes/:_id', JokesController.findOneJoke);
+    app.get('/api/jokes/random', JokesController.findOneRandomJoke);
+    app.post('/api/jokes/new', JokesController.createNewJoke);
+    app.put('/api/jokes/update/:_id', JokesController.updateJoke);
+    app.delete('/api/jokes/delete/:_id', JokesController.deleteJoke);
+}

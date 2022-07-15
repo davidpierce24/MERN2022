4a4e1a1db4bae1c50004a1d66b0b4e19d4381acb
davidpierce24 <pie2ce@gmail.com>
2022-07-15 11:56:06 -0400

    updates

--- ClassDemos/express-demo/server.js
+++ ClassDemos/express-demo/server.js
@@ -3,6 +3,18 @@ const express = require('express');
 const app = express();
 // we are going to set up a port
 const port = 8000;
+// you need these lines of code for post requests to work
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+
+// this is the example database
+const users = [
+    {name:"Dave", favoriteLanguage: 'C#'},
+    {name:"Sean", favoriteLanguage: 'Javascript'},
+    {name:"Hiyaw", favoriteLanguage: 'C#'}
+]
+
+
 
 app.get('/', (req, res) => {
     res.json({message: 'hello from the other side'})
@@ -10,6 +22,13 @@ app.get('/', (req, res) => {
 
 app.get('/example', (req, res) => {
     res.json({message:"hello from the example page", number: 8})
+})
+
+app.post('/post', (req, res) => {
+    console.log(req.body);
+    users.push(req.body)
+    // this must be here or postman hangs forever
+    res.json(users)
 })
 
 

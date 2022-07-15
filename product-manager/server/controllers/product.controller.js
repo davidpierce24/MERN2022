70996c89330fcc53594bac002eec7550011a61a7
davidpierce24 <pie2ce@gmail.com>
2022-07-15 17:48:05 -0400

    updates

--- product-manager/server/controllers/product.controller.js
+++ product-manager/server/controllers/product.controller.js
@@ -1,1 +1,12 @@
 const Product = require('../models/product.model');
+
+module.exports.createProduct = (req, res) => {
+    const { title, price, description } = req.body;
+    Product.create({
+        title,
+        price,
+        description
+    })
+        .then(product => res.json(product))
+        .catch(err => res.json(err))
+}

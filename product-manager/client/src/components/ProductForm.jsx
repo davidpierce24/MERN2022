e4944cca739c4b9966b044cfd4f8cc441e421bf4
davidpierce24 <pie2ce@gmail.com>
2022-07-16 02:02:27 -0400

    updates

--- product-manager/client/src/components/ProductForm.jsx
+++ product-manager/client/src/components/ProductForm.jsx
@@ -5,7 +5,9 @@ const ProductForm = props => {
 
     return (
         <div>
-            
+
         </div>
     )
 }
+
+export default ProductForm;
---
+++ product-manager/client/src/views/Main.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+import ProductForm from "../components/ProductForm";
+
+const Main = props => {
+
+    return (
+        <div>
+            <ProductForm />
+        </div>
+    )
+}
+
+export default Main;

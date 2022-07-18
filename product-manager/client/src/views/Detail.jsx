dd6268d82d0548d2bc7f31bceada82e2d55e3478
davidpierce24 <pie2ce@gmail.com>
2022-07-18 02:58:13 -0400

    updates

--- product-manager/client/src/views/Detail.jsx
+++ product-manager/client/src/views/Detail.jsx
@@ -1,11 +1,12 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, useNavigate } from "react-router-dom";
 
 
 const Detail = props => {
     const [product, setProduct] = useState({})
     const { id } = useParams();
+    const navigate = useNavigate();
 
     useEffect(() => {
         axios.get('http://localhost:8000/api/products/' + id)
@@ -13,12 +14,20 @@ const Detail = props => {
             .catch(err => console.log(err))
     }, [product]);
 
+    const deleteProduct = (e, id) => {
+        e.preventDefault();
+        axios.delete('http://localhost:8000/api/products/' + id)
+            .then(res => props.removeFromDom(id))
+            .catch(err => console.log(err))
+        navigate('/products');
+    }
+
     return (
         <div>
             <p>Title: {product.title}</p>
             <p>Price: ${product.price}</p>
             <p>Description: {product.description}</p>
-            <button> <Link to={`/products/${id}/edit`}>Edit</Link> </button><br />
+            <button> <Link to={`/products/${id}/edit`}>Edit</Link> </button><button onClick={e => deleteProduct(e, product._id)} >Delete</button><br />
             <Link to={`/products`}>Home</Link>
         </div>
     )

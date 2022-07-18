fd47d7ab4f5f445448679bf83f5786b3561362cd
davidpierce24 <pie2ce@gmail.com>
2022-07-18 01:45:00 -0400

    updates

--- product-manager/client/src/App.jsx
+++ product-manager/client/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Main from './views/Main';
+import Detail from './views/Detail';
 
 
 function App() {
   return (
     <div className="App">
-      <Main />
+      <Routes>
+        <Route element={<Main />} path="/products" />
+        <Route element={<Detail />} path="/products/:id" />
+      </Routes>
     </div>
   );
 }
---
+++ product-manager/client/src/views/Detail.jsx
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+
+const Detail = props => {
+    const [product, setProduct] = useState({})
+    const { id } = useParams();
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/products/' + id)
+            .then(res => setProduct(res.data))
+            .catch(err => console.log(err))
+    }, []);
+
+    return (
+        <div>
+            <p>{product.title}</p>
+            <p>{product.price}</p>
+            <p>{product.description}</p>
+        </div>
+    )
+}
+
+export default Detail;
